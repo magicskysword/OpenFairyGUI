fe514a82113f23c120a3859f1892ea0248631c56
MagicSkySword <skyswordkill@qq.com>
2026-07-28 15:58:07 +0800

    chore: 发布 OpenFairyGUI 0.2.5

--- packages/core/test/package-identity.test.ts
+++ packages/core/test/package-identity.test.ts
@@ -19,12 +19,12 @@ test('fork packages expose stable magicskysword names and semver dependencies', 
 		url: 'https://github.com/magicskysword/OpenFairyGUI/issues',
 	});
 	t.is(core.name, '@magicskysword/openfairygui-core');
-	t.is(core.version, '0.2.4');
+	t.is(core.version, '0.2.5');
 	t.is(functions.name, '@magicskysword/openfairygui-functions');
-	t.is(functions.version, '0.2.4');
+	t.is(functions.version, '0.2.5');
 	t.is(
 		functions.dependencies['@magicskysword/openfairygui-core'],
-		'^0.2.4',
+		'^0.2.5',
 		'published manifests must use a real SemVer range instead of a sibling path or workspace protocol',
 	);
 });
